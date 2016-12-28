2768b4190c4e53557fe7d5e28ea37252955b2980
pinkdevelops <pinkdevelops@gmail.com>
2016-12-28 08:27:36 -0500

    updated Google-Nifi versions.  Appending Geocoding to Flowfile content

--- nifi-googlegeocode-processors/src/main/java/org/apache/nifi/processors/googlegeocode/GoogleGeocode.java
+++ nifi-googlegeocode-processors/src/main/java/org/apache/nifi/processors/googlegeocode/GoogleGeocode.java
@@ -1,8 +1,20 @@
-package org.apache.nifi.processors.googlegeocode;
-
-/**
- * Created by dpinkston on 6/24/16.
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+package org.apache.nifi.processors.googlegeocode;
 
 import com.google.maps.GeoApiContext;
 import com.google.maps.GeocodingApi;
@@ -19,6 +31,7 @@ import org.apache.nifi.processor.*;
 import org.apache.nifi.processor.exception.ProcessException;
 import org.apache.nifi.processor.io.InputStreamCallback;
 import org.apache.nifi.processor.io.OutputStreamCallback;
+import org.apache.nifi.processor.io.StreamCallback;
 import org.apache.nifi.processor.util.StandardValidators;
 import org.apache.nifi.processors.googlegeocode.util.GoogleGeocodeURLBuilder;
 import org.json.JSONArray;
@@ -119,12 +132,9 @@ public class GoogleGeocode extends AbstractProcessor {
             return;
         }
 
-        final String addressAttributeName = context.getProperty(ADDRESS_ATTRIBUTE).getValue();
-        final String addressAttributeValue = flowFile.getAttribute(addressAttributeName);
-        final String cityAttributeName = context.getProperty(CITY_ATTRIBUTE).getValue();
-        final String cityAttributeValue = flowFile.getAttribute(cityAttributeName);
-        final String stateAttributeName = context.getProperty(STATE_ATTRIBUTE).getValue();
-        final String stateAttributeValue = flowFile.getAttribute(stateAttributeName);
+        final String addressAttributeValue = flowFile.getAttribute(context.getProperty(ADDRESS_ATTRIBUTE).getValue());
+        final String cityAttributeValue = flowFile.getAttribute(context.getProperty(CITY_ATTRIBUTE).getValue());
+        final String stateAttributeValue = flowFile.getAttribute(context.getProperty(STATE_ATTRIBUTE).getValue());
         final String apiKeyAttributeValue = context.getProperty(GOOGLE_GEOCODE_API_KEY).getValue();
 
         GoogleGeocodeURLBuilder urlBuilder = new GoogleGeocodeURLBuilder(addressAttributeValue, cityAttributeValue,
@@ -147,35 +157,15 @@ public class GoogleGeocode extends AbstractProcessor {
             return;
         }
 
-        final GeocodingResult[] finalResults = results;
-        session.read(flowFile, new InputStreamCallback() {
+        flowFile = session.write(flowFile, new StreamCallback() {
             @Override
-            public void process(InputStream in) throws IOException {
-                try {
-                    String inputJson = IOUtils.toString(in);
-                    json[0] = new JSONObject(inputJson);
-                    JSONObject locationType = new JSONObject();
-                    locationType.put("type", "Point");
-                    JSONArray coordinates = new JSONArray();
-                    coordinates.put(finalResults[0].geometry.location);
-                    locationType.put("coordinates: ", coordinates);
-                    json[0].put("location_1", locationType);
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                    getLogger().error("Failed to read json string.");
-                }
+            public void process(final InputStream in, final OutputStream out) throws IOException {
+                JSONObject obj = new JSONObject(IOUtils.toString(in));
+                obj.put("formatted_address", results[0].formattedAddress);
+                obj.put("location", results[0].geometry.location);
+                out.write(obj.toString().getBytes());
             }
         });
-
-        // To write the results back out ot flow file
-        flowFile = session.write(flowFile, new OutputStreamCallback() {
-
-            @Override
-            public void process(OutputStream out) throws IOException {
-                out.write(json[0].toString().getBytes());
-            }
-        });
-
         session.transfer(flowFile, REL_FOUND);
     }
 }
